fa97f939d5affeff1040b5fa7a5d4271853bf86f
edonapllana <edona.pllana0@gmail.com>
2022-08-21 00:40:18 +0200

    adding express app, config of dot env, and use app extend json, urlencode and cors

--- back/server.js
+++ back/server.js
@@ -2,3 +2,10 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cors = require(cors);
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({extended: true}));
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
