38c9334baee56715f7df0e4a7b89098923fc3388
edonapllana <edona.pllana0@gmail.com>
2022-08-30 13:56:05 +0200

    making api and functions, starting to connect back side with front one

--- front/src/App.js
+++ front/src/App.js
@@ -3,26 +3,40 @@ import Preloader from "../components/Preloader";
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <form className="col s12">
-          <div className="row">
-            <div className="input-field col s6">
-              <i className="material-icons prefix">title</i>
-              <input id="icon_prefix" type="text" className="validate" />
-              <label htmlFor="icon_prefix">Title</label>
-            </div>
-            <div className="input-field col s6">
-              <i className="material-icons prefix">description</i>
-              <input id="description" type="tel" className="validate" />
-              <label htmlFor="icon_telephone">content</label>
-            </div>
-          </div>
-        </form>
-        <Preloader/>
-      </div>
-    </div>
-  );
+		<div className="container">
+			<div className="row">
+				<form className="col s12">
+					<div className="row">
+						<div className="input-field col s6">
+							<i className="material-icons prefix">title</i>
+							<input id="icon_prefix" type="text" className="validate" />
+							<label htmlFor="icon_prefix">Title</label>
+						</div>
+						<div className="input-field col s6">
+							<i className="material-icons prefix">description</i>
+							<input id="description" type="tel" className="validate" />
+							<label htmlFor="icon_telephone">content</label>
+						</div>
+					</div>
+				</form>
+				<Preloader />
+				<div class="collection">
+					<a href="#!" class="collection-item">
+						Alvin
+					</a>
+					<a href="#!" class="collection-item active">
+						Alvin
+					</a>
+					<a href="#!" class="collection-item">
+						Alvin
+					</a>
+					<a href="#!" class="collection-item">
+						Alvin
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
